6c0a4f2cb09f52e2f997d85053c5720155b8f424
Joshua Peek <josh@joshpeek.com>
2015-06-13 12:35:41 -0700

    Remove IIFE

--- Ka-Block.safariextension/filter.js
+++ Ka-Block.safariextension/filter.js
@@ -1,39 +1,37 @@
-(function() {
-  // Match literal dots in domain names.
-  function escape(domain) {
-    return domain.replace(/[.]/g, '\\.')
-  }
+// Match literal dots in domain names.
+function escape(domain) {
+  return domain.replace(/[.]/g, '\\.')
+}
 
-  // Get trigger url for block list rule
-  function ruleURL(rule) {
-    return rule.trigger['url-filter']
-  }
+// Get trigger url for block list rule
+function ruleURL(rule) {
+  return rule.trigger['url-filter']
+}
 
-  // Create filter predicate function for blocked domains.
-  function filter(rules) {
-    const all = rules.map(ruleURL).map(escape).join('|')
-    const re = new RegExp('(?:^|\\.)(?:' + all + ')$', 'i')
-    return re.test.bind(re)
-  }
+// Create filter predicate function for blocked domains.
+function filter(rules) {
+  const all = rules.map(ruleURL).map(escape).join('|')
+  const re = new RegExp('(?:^|\\.)(?:' + all + ')$', 'i')
+  return re.test.bind(re)
+}
 
-  // Respond to approval requests from start script.
-  function installCanLoad(rules) {
-    const blocked = filter(rules)
-    safari.application.addEventListener('message', function(event) {
-      if (event.name === 'canLoad') {
-        const host = new URL(event.message).hostname
-        event.message = blocked(host) ? 'block' : 'allow'
-      }
-    }, true)
-  }
+// Respond to approval requests from start script.
+function installCanLoad(rules) {
+  const blocked = filter(rules)
+  safari.application.addEventListener('message', function(event) {
+    if (event.name === 'canLoad') {
+      const host = new URL(event.message).hostname
+      event.message = blocked(host) ? 'block' : 'allow'
+    }
+  }, true)
+}
 
-  function init() {
-    const req = new XMLHttpRequest()
-    req.responseType = 'json'
-    req.onload = function() { installCanLoad(req.response) }
-    req.open('GET', 'blockerList.json', true)
-    req.send()
-  }
+function init() {
+  const req = new XMLHttpRequest()
+  req.responseType = 'json'
+  req.onload = function() { installCanLoad(req.response) }
+  req.open('GET', 'blockerList.json', true)
+  req.send()
+}
 
-  init()
-})()
+init()
