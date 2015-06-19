029a055911ec7cb43d1c04d4ed11f6dca84349e9
Joshua Peek <josh@joshpeek.com>
2015-06-18 19:36:55 -0700

    Add iOS Ka-Block

---
+++ Ka-Block Content Blocker/ActionRequestHandler.swift
@@ -0,0 +1,13 @@
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+
+        context.completeRequestReturningItems([item], completionHandler: nil)
+    }
+}
