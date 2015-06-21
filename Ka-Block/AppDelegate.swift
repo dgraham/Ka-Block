86db41efd525eef7d71ef052bd09cc928e8452d0
Joshua Peek <josh@joshpeek.com>
2015-06-20 19:47:22 -0700

    Add basic enabled/disabled label

--- Ka-Block/AppDelegate.swift
+++ Ka-Block/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SafariServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,5 +7,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
+    }
+
+    var extensionEnabled = false {
+        didSet {
+            if let viewController = self.window?.rootViewController as? ViewController {
+                viewController.extensionEnabled = extensionEnabled
+            }
+        }
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        extensionEnabled = false
+        SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.github.dgraham.Ka-Block.Ka-Block-Content-Blocker") { error in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.extensionEnabled = true
+            }
+        }
     }
 }
--- Ka-Block/ViewController.swift
+++ Ka-Block/ViewController.swift
@@ -1,4 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+
+    var extensionEnabled = false {
+        didSet {
+            if extensionEnabled {
+                self.label.text = "Enabled"
+            } else {
+                self.label.text = "Disabled"
+            }
+        }
+    }
 }
