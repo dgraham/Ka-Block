9e965f94b9ca4badce291dbb780690297c3a9bb1
Joshua Peek <josh@joshpeek.com>
2015-08-25 22:08:49 -0700

    Remove broken extension enabled detection

--- Ka-Block/AppDelegate.swift
+++ Ka-Block/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import SafariServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,22 +6,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
-    }
-
-    var extensionEnabled = false {
-        didSet {
-            if let viewController = self.window?.rootViewController as? ViewController {
-                viewController.extensionEnabled = extensionEnabled
-            }
-        }
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        extensionEnabled = false
-        SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.kablock.Ka-Block.Ka-Block-Content-Blocker") { error in
-            dispatch_async(dispatch_get_main_queue()) {
-                self.extensionEnabled = true
-            }
-        }
     }
 }
--- Ka-Block/ViewController.swift
+++ Ka-Block/ViewController.swift
@@ -3,16 +3,6 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
 
-    var extensionEnabled = false {
-        didSet {
-            if extensionEnabled {
-                self.label.text = "Ad blocking is enabled.\nYou’re all set!"
-            } else {
-                self.label.text = "Enable the Safari extension\nto prevent ads from appearing\non web pages."
-            }
-        }
-    }
-
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent;
     }
