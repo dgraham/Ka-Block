3066cad355c91480959516b4fcbc7b65d513e0f6
David Graham <david.malcom.graham@gmail.com>
2016-09-11 09:13:15 -0600

    Detect extension enabled status on iOS 10

--- Ka-Block/ViewController.swift
+++ Ka-Block/ViewController.swift
@@ -1,9 +1,28 @@
+import SafariServices
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var disabledLabel: UILabel!
+    @IBOutlet weak var enabledLabel: UILabel!
 
     override var preferredStatusBarStyle : UIStatusBarStyle {
         return UIStatusBarStyle.lightContent;
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        if #available(iOS 10.0, *) {
+            SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: "com.kablock.ios.Ka-Block-Content-Blocker", completionHandler: {
+                (state, error) in
+
+                if state != nil {
+                    self.enabledLabel.isHidden = !state!.isEnabled
+                    self.disabledLabel.isHidden = state!.isEnabled
+                }
+            })
+        } else {
+            // Show settings hint on iOS 9.
+            self.enabledLabel.isHidden = true
+            self.disabledLabel.isHidden = false
+        }
     }
 }
