f2403a833a54c833de4cde1ec982d790f64a8130
David Graham <david.malcom.graham@gmail.com>
2017-10-07 15:48:01 -0600

    Require iOS 10.0

--- Ka-Block/iOS/ViewController.swift
+++ Ka-Block/iOS/ViewController.swift
@@ -20,21 +20,15 @@ class ViewController: UIViewController {
     }
     
     @objc func checkEnabled() {
-        if #available(iOS 10.0, *) {
-            SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: "com.kablock.ios.Ka-Block-Content-Blocker", completionHandler: {
-                (state, error) in
-                
-                if state != nil {
-                    DispatchQueue.main.async {
-                        self.enabledLabel.isHidden = !state!.isEnabled
-                        self.disabledLabel.isHidden = state!.isEnabled
-                    }
+        SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: "com.kablock.ios.Ka-Block-Content-Blocker", completionHandler: {
+            (state, error) in
+
+            if state != nil {
+                DispatchQueue.main.async {
+                    self.enabledLabel.isHidden = !state!.isEnabled
+                    self.disabledLabel.isHidden = state!.isEnabled
                 }
-            })
-        } else {
-            // Show settings hint on iOS 9.
-            self.enabledLabel.isHidden = true
-            self.disabledLabel.isHidden = false
-        }
+            }
+        })
     }
 }
