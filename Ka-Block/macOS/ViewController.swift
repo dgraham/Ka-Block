42912cbf0d865f7c19cb8321eae8434d361fc3e0
David Graham <david.malcom.graham@gmail.com>
2017-12-20 20:58:32 -0700

    Use logo to represent enabled state

--- Ka-Block/macOS/ViewController.swift
+++ Ka-Block/macOS/ViewController.swift
@@ -1,11 +1,18 @@
 import Cocoa
 
 class ViewController: NSViewController {
-    @IBOutlet weak var label: NSTextField!
+    @IBOutlet weak var enabledLabel: NSTextField!
+    @IBOutlet weak var disabledLabel: NSTextField!
+    @IBOutlet weak var enabledImage: NSImageView!
+    @IBOutlet weak var disabledImage: NSImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let isEnabled = true
+        self.enabledLabel.isHidden = !isEnabled
+        self.disabledLabel.isHidden = isEnabled
+        self.enabledImage.isHidden = !isEnabled
+        self.disabledImage.isHidden = isEnabled
     }
 
     override var representedObject: Any? {
