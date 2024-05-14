f912befa3ae6b0fc700675d379d1d03f85aa02c6
Joshua Peek <josh@joshpeek.com>
2024-05-13 18:51:37 -0700

    Remove hacks and now just use windowResizability

--- App/KaBlockApp.swift
+++ App/KaBlockApp.swift
@@ -4,65 +4,16 @@ import SwiftUI
 struct KaBlockApp: App {
     @ObservedObject var contentBlockerState = ContentBlockerState(withIdentifier: "com.kablock.ios.KaBlockContentBlocker")
 
-    #if os(macOS)
-        @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    #endif
-
     var body: some Scene {
-        #if os(iOS)
-            WindowGroup {
-                HomeScreen(isEnabled: contentBlockerState.isEnabled)
-            }
-        #elseif os(macOS)
-            WindowGroup {
-                HomeScreen(isEnabled: contentBlockerState.isEnabled)
-                    .frame(width: 375, height: 640)
-                    .background(NonResizableWindow())
-            }
-            .windowStyle(HiddenTitleBarWindowStyle())
+        WindowGroup {
+            HomeScreen(isEnabled: contentBlockerState.isEnabled)
+            #if os(macOS)
+                .frame(width: 375, height: 640)
+            #endif
+        }
+        #if os(macOS)
+        .windowResizability(.contentSize)
+        .windowStyle(.hiddenTitleBar)
         #endif
     }
 }
-
-#if os(macOS)
-    struct NonResizableWindow: NSViewRepresentable {
-        func makeNSView(context _: Context) -> NSView {
-            let view = NSView()
-            DispatchQueue.main.async {
-                guard let window = view.window else { return }
-                window.styleMask = [.closable, .titled, .fullSizeContentView]
-                window.collectionBehavior = [.fullScreenNone]
-
-                if let menu = window.menu {
-                    let appMenuItem = menu.item(withTitle: "Ka-Block!")!
-                    let fileMenuItem = menu.item(withTitle: "File")!
-
-                    menu.items = [appMenuItem, fileMenuItem]
-
-                    appMenuItem.submenu!.items = [
-                        appMenuItem.submenu!.item(withTitle: "About Ka-Block!")!,
-                        NSMenuItem.separator(),
-                        appMenuItem.submenu!.item(withTitle: "Quit Ka-Block!")!,
-                    ]
-
-                    fileMenuItem.submenu!.items = [
-                        fileMenuItem.submenu!.item(withTitle: "Close")!,
-                    ]
-                }
-            }
-            return view
-        }
-
-        func updateNSView(_: NSView, context _: Context) {}
-    }
-
-    class AppDelegate: NSObject, NSApplicationDelegate {
-        func applicationDidFinishLaunching(_: Notification) {
-            NSWindow.allowsAutomaticWindowTabbing = false
-        }
-
-        func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
-            true
-        }
-    }
-#endif
